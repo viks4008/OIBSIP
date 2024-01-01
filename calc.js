db8d13286fa22746c6de6ff2116f0de68c1d55ef
Vikram Dilip Dhole <86395338+viks4008@users.noreply.github.com>
2024-01-01 22:38:49 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,28 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let result = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+
+        if (e.target.innerHTML == '=') {
+            if (result != "") {
+                result = eval(result);
+                input.value = result;
+            }
+        }
+        else if (e.target.innerHTML == 'AC') {
+            result = "";
+            input.value = result;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            result = result.substring(0, result.length - 1);
+            input.value = result;
+        }
+        else {
+            result += e.target.innerHTML;
+            input.value = result;
+        }
+    })
+})
